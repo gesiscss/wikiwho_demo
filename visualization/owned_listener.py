3a3e194f8f01c4068d8d63991c22688de05d288a
Roberto Ulloa <roberto.ur@gmail.com>
2019-01-19 10:43:37 +0100

    the two series of the tokens owned now run in paralell

--- visualization/owned_listener.py
+++ visualization/owned_listener.py
@@ -1,27 +1,67 @@
 import pandas as pd
 import plotly
 from plotly import graph_objs
-
+import datetime
 
 class OwnedListener():
 
     def __init__(self, df, editor):
         self.df = df.sort_values(['token_id', 'rev_time'], ascending=True).set_index('token_id')
         self.editor = editor
+        self.days = days = pd.Series(df.loc[df['o_editor'] == editor, 'rev_time'].dt.to_period('D').unique()).sort_values(ascending=False)
+
+        days = self.days.dt.to_timestamp('D') + pd.DateOffset(1)
+
+        _all = []
+        _abs = []
+        df = self.df
+        for rev_time in days:
+            df = df[df['rev_time'] <= rev_time]
+            last_action = df.groupby('token_id').last()
+            surv = last_action[last_action['action'] != 'out']
+            _abs.append(sum(surv['o_editor'] == self.editor))
+            _all.append(len(surv))
+
+        self.summ = pd.DataFrame({
+            'day': days,
+            'abs': _abs,
+            'all': _all
+            })
+        self.summ['res'] = 100 * self.summ['abs'] / self.summ['all']
+
         self.df_plotted = None
 
     def listen(self, _range, granularity, trace):
         df = self.df
 
         df = df[(df.rev_time.dt.date >= _range[0]) &
-                (df.rev_time.dt.date <= _range[1])]
+                (df.rev_time.dt.date <= _range[1] + datetime.timedelta(days=1))]
 
-        self.doi = df.loc[df['editor'] == self.editor, 'rev_time'].dt.to_period(
-            granularity[0]).dt.to_timestamp(granularity[0]).sort_values(ascending=False).unique()
-
+        self.doi = pd.Series(self.days.dt.to_timestamp(granularity[0]).unique()) + pd.DateOffset(1)
         self.traces = []
         self.is_norm_scale = True
-        df = self.__add_trace(df, trace, 'rgba(0, 0, 0, 1)')
+
+        if trace == 'Tokens Owned':
+            self.is_norm_scale = False
+            _df = self.summ
+            _df['time'] = _df['day'].dt.to_period(granularity[0]).dt.to_timestamp(granularity[0])
+            _df = _df[~_df.duplicated(subset='time', keep='first')]
+            _y = _df['abs']
+
+        elif trace == 'Tokens Owned (%)':
+            _df = self.summ
+            _df['time'] = _df['day'].dt.to_period(granularity[0]).dt.to_timestamp(granularity[0])
+            _df = _df[~_df.duplicated(subset='time', keep='first')]
+            _y = _df['res']
+            
+        self.traces.append(
+            graph_objs.Scatter(
+                x=_df['time'], y=_y,
+                name=trace,
+                marker=dict(color='rgba(255, 0, 0, .5)'))
+        )
+
+        self.__add_trace(df, trace, 'rgba(0,0,255, .5)')
 
         _range = None
         if self.is_norm_scale:
@@ -61,7 +101,7 @@ class OwnedListener():
 
         self.traces.append(
             graph_objs.Scatter(
-                x=pd.Series(self.doi), y=_y,
+                x=self.doi, y=_y,
                 name=trace,
                 marker=dict(color=color))
         )
